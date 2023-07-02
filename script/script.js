e42fde8ec17802e99d9cfe559188f553acddb312
aleks-123 <ace.ki@hotmail.com>
2023-07-03 01:05:35 +0200

    version 2.4

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $(".hamburger").on("click", function () {
+    console.log("clicked");
     $(this).toggleClass("active");
     $(".nav-menu").toggleClass("active");
   });
@@ -8,10 +9,11 @@ $(document).ready(function () {
     $(".hamburger").removeClass("active");
     $(".nav-menu").removeClass("active");
   });
+
   document.querySelectorAll(".nav-link").forEach((n) =>
     n.addEventListener("click", () => {
-      hamburger.classList.remove("active");
-      navMenu.classList.remove("active");
+      document.querySelector(".hamburger").classList.remove("active");
+      document.querySelector(".nav-menu").classList.remove("active");
     })
   );
 
